33ba117772300a35442605db64d34dd01971f33a
Akanho Yusuf <akanhoyusuf.official@gmail.com>
2026-04-16 17:27:29 +0100

    port changed

--- main.go
+++ main.go
@@ -1,20 +1,45 @@
+// package main
+
+// import (
+// 	"ascii-art-web/handler"
+// 	"fmt"
+// 	"net/http"
+// )
+
+// func main() {
+// 	http.HandleFunc("/", handler.HomeHandler)
+// 	http.HandleFunc("/asciiart", handler.AsciiHomeHandler)
+
+// 	fmt.Println("Server running at http://localhost:8080")
+// 	//http.ListenAndServe(":8080", nil)
+// 	err := http.ListenAndServe(":8080", nil)
+// if err != nil {
+// 	fmt.Println("Server error:", err)
+
+	 
+// }
+// }
 package main
 
 import (
 	"ascii-art-web/handler"
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", handler.HomeHandler)
 	http.HandleFunc("/asciiart", handler.AsciiHomeHandler)
 
-	fmt.Println("Server running at http://localhost:8080")
-	//http.ListenAndServe(":8080", nil)
-	err := http.ListenAndServe(":8080", nil)
-if err != nil {
-	fmt.Println("Server error:", err)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
-}
+	fmt.Println("Server running on port:", port)
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		fmt.Println("Server error:", err)
+	}
 }
